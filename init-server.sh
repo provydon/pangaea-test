adcd4f7171e6a2cfcfe394dc2c3f9cf92a25b736
Providence ifeosame <providenceifeosame@gmail.com>
2021-08-22 21:58:46 +0100

    update

---
+++ init-server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "initializing subscriber"
+cd subscriber
+echo "install dependencies"
+sudo composer install &
+sudo composer dump-autoload &
+
+echo "creating new .env form .env.example."
+cp .env.example .env
+
+echo "subscriber Application Key Set"
+sudo php artisan key:generate &
+
+echo "subscriber Migration Started"
+sudo php artisan migrate &
+echo "subscriber migration Finished Successfuly"
+
+echo "subscriber server set"
+
+cd -
+
+echo "initializing publisher"
+cd publisher
+echo "install dependencies"
+sudo composer install &
+sudo composer dump-autoload &
+
+echo "creating new .env form .env.example."
+cp .env.example .env
+
+echo "publisher Application Key Set"
+sudo php artisan key:generate &
+
+echo "publisher Migration Started"
+sudo php artisan migrate &
+echo "publisher migration Finished Successfuly"
+
+echo "publisher server set"
--- start-server.sh
+++ start-server.sh
@@ -3,8 +3,9 @@
 echo "starting subscriber" 
 cd subscriber
 echo "install dependencies" 
-sudo composer install &
+sudo composer update &
 sudo composer dump-autoload &
+
 # check for .env
 FILE=.env
 if [ -f "$FILE" ]; then
@@ -12,16 +13,22 @@ if [ -f "$FILE" ]; then
 else 
     echo "subscriber $FILE does not exist, creating new .env form .env.example."
     cp .env.example .env
+    echo "subscriber Application Key Set"
+    sudo php artisan key:generate &
 fi
 
-
-echo "subscriber Application Key Set"
-sudo php artisan key:generate &
-echo "subscriber Server Ready"
-sudo php artisan serve --port=7000 &
 echo "subscriber Migration Started" 
 sudo php artisan migrate &
 echo "subscriber migration Finished Successfuly" 
+
+echo "start server"
+if lsof -Pi :7080 -sTCP:LISTEN -t >/dev/null ; then
+    kill $(sudo lsof -t -i:7000)
+    sudo php artisan serve --port=7000 &
+else
+    sudo php artisan serve --port=7000 &
+fi
+echo "subscriber Server Ready"
 echo "subscriber server is running on port 7000" 
 
 cd -
@@ -29,8 +36,9 @@ cd -
 echo "starting publisher" 
 cd publisher
 echo "install dependencies" 
-sudo composer install &
+sudo composer update &
 sudo composer dump-autoload &
+
 # check for .env
 FILE=.env
 if [ -f "$FILE" ]; then
@@ -38,16 +46,22 @@ if [ -f "$FILE" ]; then
 else 
     echo "publisher $FILE does not exist, creating new .env form .env.example."
     cp .env.example .env
+    echo "publisher Application Key Set"
+    sudo php artisan key:generate &
 fi
 
-
-echo "publisher Application Key Set"
-sudo php artisan key:generate &
-echo "publisher Server Ready"
-sudo php artisan serve --port=8000 &
 echo "publisher Migration Started" 
 sudo php artisan migrate &
 echo "publisher migration Finished Successfuly" 
+
+echo "publisher Server Ready"
+if lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null ; then
+    kill $(sudo lsof -t -i:7000)
+    sudo php artisan serve --port=8000 &
+else
+    sudo php artisan serve --port=8000 &
+fi
+
 echo "publisher Queue Started"
 sudo php artisan queue:work --daemon --timeout=3000 &
 echo "publisher server is running on port 8000" 
